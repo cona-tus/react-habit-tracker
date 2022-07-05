f61edb59b22b56da6949c72f73062cbbc697d993
Conatus <cona_tus@naver.com>
2022-07-05 13:59:55 +0900

    Create Navbar Component

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  // prop으로 전달받은 습관의 데이터를 보여주기만 하는 컴포넌트이기 때문에 state를 가지고 있을 필요가 없음.
-  //   state = {
-  //     count: 0,
-  //   };
-
   handleIncrement = () => {
     this.props.onIncrement(this.props.habit);
-    //    state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야 함.
-    //    this.state.count += 1; 리액트에서는 업데이트 됐는 지 모름.
-    //    this.setState({ count: this.state.count + 1 });
   };
 
   handleDecrement = () => {
     this.props.onDecrement(this.props.habit);
-    //    const count = this.state.count - 1;
-    //    this.setState({ count: count < 0 ? 0 : count });
   };
 
   handleDelete = () => {
@@ -24,8 +14,6 @@ class Habit extends Component {
   };
 
   render() {
-    // console.log(this.props.habit);
-    // const habitName = this.props.habit.name;
     const { name, count } = this.props.habit;
     return (
       <li className='habit'>

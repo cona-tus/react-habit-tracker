8d3daf654d19aa15d0e2e9258842e7ff6266dabc
Conatus <cona_tus@naver.com>
2022-07-05 13:41:43 +0900

    State functions

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
+  // prop으로 전달받은 습관의 데이터를 보여주기만 하는 컴포넌트이기 때문에 state를 가지고 있을 필요가 없음.
+  //   state = {
+  //     count: 0,
+  //   };
 
   handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야 함.
-    // this.state.count += 1; 리액트에서는 업데이트 됐는 지 모름.
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
+    //    state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야 함.
+    //    this.state.count += 1; 리액트에서는 업데이트 됐는 지 모름.
+    //    this.setState({ count: this.state.count + 1 });
   };
+
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
+    //    const count = this.state.count - 1;
+    //    this.setState({ count: count < 0 ? 0 : count });
   };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
   render() {
+    // console.log(this.props.habit);
+    // const habitName = this.props.habit.name;
+    const { name, count } = this.props.habit;
     return (
       <li className='habit'>
-        <span className='habit-name'>Reading</span>
-        <span className='habit-count'>{this.state.count}</span>
+        <span className='habit-name'>{name}</span>
+        <span className='habit-count'>{count}</span>
         <button
           className='habit-button habit-increase'
           onClick={this.handleIncrement}
@@ -31,7 +43,10 @@ class Habit extends Component {
         >
           <i className='fas fa-minus-square'></i>
         </button>
-        <button className='habit-button habit-delete'>
+        <button
+          className='habit-button habit-delete'
+          onClick={this.handleDelete}
+        >
           <i className='fas fa-trash'></i>
         </button>
       </li>
